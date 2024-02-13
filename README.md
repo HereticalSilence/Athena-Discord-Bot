40d084bd9ce0da95b7e23911532cf18d3306a5da
Miles Armé <52762433+HereticalSilence@users.noreply.github.com>
2024-02-13 02:06:21 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Athena-Discord-Bot
+ Athena the discord bot. Written in Python 3 using the Discord API Wrapper Discord.py version 2.3.2
---
+++ main.py
@@ -0,0 +1,70 @@
+#-- Import Discord Application Tokens --#
+import secret
+
+#-- Import Discord Related Packages --#
+import discord
+from discord.ext import tasks, commands
+import asyncio
+
+#-- Import SQLITE3 for database access --#
+import sqlite3
+
+#-- Additional Imports --#
+import datetime
+import random
+import os
+
+#-- Discord Intents --#
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.members = True
+intents.message_content = True
+
+#-- Bot Launcher --#
+ValidSelection = False
+while not ValidSelection:
+
+    botSelection = input("Please select a bot to launch\n0. Athena\n1. Athena Dev\n-> ")
+
+    if botSelection == "0":
+        TOKEN = secret.Athena_Token
+        prefix = "$"
+
+        bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix), owner_id=secret.OwnerID, case_insensitive=True, intents=intents)
+        
+        ValidSelection = True
+
+    elif botSelection == "1":
+        TOKEN = secret.AthenaDev_Token
+        prefix = "$$"
+
+        bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix), owner_id=secret.OwnerID, case_insensitive=True, intents=intents)
+
+        ValidSelection = True
+        
+    else:
+        print ("ERROR: Invalid Bot Selection")
+
+#-- Universal Variables --#
+bot.prefix = prefix
+bot.startTime = datetime.datetime.now()
+
+#-- Initial Cogs --#
+#This will eventually be replaced with a better way to do this
+#by loading the cogs from a database but that is for later
+initialCogs = []
+
+#-- Ready Function --#
+@bot.event
+async def on_ready():
+    for cog in initialCogs:
+        try:
+            await bot.load_extension(cog)
+        except Exception:
+            print(Exception)
+        else:
+            print(f"{cog} was loaded successfully")
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})\nStart Time: {bot.startTime}")
+
+bot.run(TOKEN)
