6ea5a3080d672c271b8e526575fba9f6f67c6983
Miles Armé <52762433+HereticalSilence@users.noreply.github.com>
2024-06-30 16:36:56 +0100

    Database inclusion update
    Bot now connects to a PostgreSQL database running locally on my server. Currently only has the boot cogs store on but will be used later for keeping track of user points etc

--- main.py
+++ main.py
@@ -4,15 +4,26 @@ import secret
 #-- Import Discord Related Packages --#
 import discord
 from discord.ext import tasks, commands
-import asyncio
 
-#-- Import SQLITE3 for database access --#
-import sqlite3
+#-- Import psycopg2 for database access --#
+import psycopg2
 
 #-- Additional Imports --#
 import datetime
-import random
-import os
+
+#-- Connect to Database Function --#
+#-- This function is used to connect to the database --#
+#-- It takes the database name as a parameter and returns the cursor and connection --#
+def connectToDatabase(Database):
+    print (f"Attempting to connect to {Database} database")
+    try:
+        connection = psycopg2.connect(user = secret.DatabaseUser, password = secret.DatabasePassword, host = secret.DatabaseHost, port = secret.DatabasePort, database = Database)
+        cursor = connection.cursor()
+        print (f"Connected to {Database} database successfully")
+        return cursor, connection
+    except (Exception, psycopg2.Error) as error:
+        print(f"Error while connecting to {Database}", error)
+
 
 #-- Discord Intents --#
 intents = discord.Intents.default()
@@ -32,7 +43,8 @@ while not ValidSelection:
         prefix = "$"
 
         bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix), owner_id=secret.OwnerID, case_insensitive=True, intents=intents)
-        
+        bot.dbCursor, bot.dbConnection = connectToDatabase("Athena")
+
         ValidSelection = True
 
     elif botSelection == 1:
@@ -40,6 +52,7 @@ while not ValidSelection:
         prefix = "$$"
 
         bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix), owner_id=secret.OwnerID, case_insensitive=True, intents=intents)
+        bot.dbCursor, bot.dbConnection = connectToDatabase("Athena-Dev")
 
         ValidSelection = True
         
@@ -50,31 +63,34 @@ while not ValidSelection:
 bot.startTime = datetime.datetime.now()
 
 #-- Remove Standard Help Command --#
-bot.remove_command('help')
+#bot.remove_command('help')
 
 #-- Universal Variables --#
 bot.prefix = prefix
 bot.startTime = datetime.datetime.now()
 
 #-- Initial Cogs --#
-#This will eventually be replaced with a better way to do this
-#by loading the cogs from a database but that is for later
-initialCogs = []
+bot.dbCursor.execute("SELECT modulename FROM modules WHERE enabled = True;")
+initialCogs = bot.dbCursor.fetchall()
+bot.dbConnection.close()
 
 #-- Load Cog Function --#
 async def loadCog(cog):
     try:
-        await bot.load_extension(cog)
+        await bot.load_extension("cogs." + cog[0])
     except Exception:
         print(Exception)
     else:
-        print(f"{cog} was loaded successfully")
+        print(f"{cog[0]} was loaded successfully")
 
 #-- Ready Function --#
 @bot.event
 async def on_ready():
+    print ("\n---------------------------------")
     for cog in initialCogs:
-        loadCog(cog)
+        await loadCog(cog)
+    print ("---------------------------------\n")
     print(f"Logged in as {bot.user} (ID: {bot.user.id})\nStart Time: {bot.startTime}")
+    
 
 bot.run(TOKEN)
